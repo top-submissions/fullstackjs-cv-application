faf5939e8487e80fd25f1497eddd7575e7e2964e
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-17 21:36:13 +0800

    feat(App): add Educational Experience form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GeneralInformationForm from './components/forms/GeneralInformationForm/GeneralInformationForm';
+import EducationalExperienceForm from './components/forms/EducationalExperienceForm/EducationalExperienceForm';
 
 function App() {
   return (
     <div>
       <h1>CV Application</h1>
       <GeneralInformationForm />
+      <EducationalExperienceForm />
     </div>
   );
 }
