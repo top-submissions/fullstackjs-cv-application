a0e1c79079a574c4294587370e1161ff4fcc8747
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-17 21:00:39 +0800

    test(form): test if Educational Experience form renders input fields

--- src/components/forms/EducationalExperienceForm/EducationalExperienceForm.test.jsx
+++ src/components/forms/EducationalExperienceForm/EducationalExperienceForm.test.jsx
@@ -6,7 +6,14 @@ describe('EducationalExperienceForm', () => {
   it('renders Educational Experience heading', () => {
     render(<EducationalExperienceForm />);
     expect(
-      screen.getByRole('heading', { name: /educational experience/i })
+      screen.getByRole('heading', { name: /educational experience/i }),
     ).toBeInTheDocument();
+  });
+
+  it('renders inputs for School Name, Title of Study, and Date of Study', () => {
+    render(<EducationalExperienceForm />);
+    expect(screen.getByLabelText(/school name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/title of study/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/date of study/i)).toBeInTheDocument();
   });
 });
