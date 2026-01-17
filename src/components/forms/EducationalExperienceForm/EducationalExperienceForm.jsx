e1d3012197b73f604fc4262b35d98bf045dfd35f
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-17 21:02:08 +0800

    feat(form): add Educational Experience form input fields

--- src/components/forms/EducationalExperienceForm/EducationalExperienceForm.jsx
+++ src/components/forms/EducationalExperienceForm/EducationalExperienceForm.jsx
@@ -2,6 +2,15 @@ function EducationalExperienceForm() {
   return (
     <div>
       <h2>Educational Experience</h2>
+      <label>
+        School Name: <input type="text" />
+      </label>
+      <label>
+        Title of Study: <input type="text" />
+      </label>
+      <label>
+        Date of Study: <input type="text" />
+      </label>
     </div>
   );
 }
