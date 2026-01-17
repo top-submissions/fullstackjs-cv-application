80ed2fe87356cd971e6b5f453dbe2d13aa4b7a14
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-18 06:27:59 +0800

    test(form): test if Practical Experience form renders input fields

--- src/components/forms/PracticalExperienceForm/PracticalExperienceForm.test.jsx
+++ src/components/forms/PracticalExperienceForm/PracticalExperienceForm.test.jsx
@@ -9,4 +9,11 @@ describe('PracticalExperienceForm', () => {
       screen.getByRole('heading', { name: /practical experience/i }),
     ).toBeInTheDocument();
   });
+
+  it('renders input fields for Company Name, Position Title, and Date of Employment', () => {
+    render(<PracticalExperienceForm />);
+    expect(screen.getByLabelText(/company name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/position title/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/date of employment/i)).toBeInTheDocument();
+  });
 });
