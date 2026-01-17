a98c6efdba80cfd52c33cfad5ea8c8edd29468a0
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-18 06:28:16 +0800

    feat(form): add form input fields

--- src/components/forms/PracticalExperienceForm/PracticalExperienceForm.jsx
+++ src/components/forms/PracticalExperienceForm/PracticalExperienceForm.jsx
@@ -2,6 +2,15 @@ function PracticalExperienceForm() {
   return (
     <div>
       <h2>Practical Experience</h2>
+      <label>
+        Company Name: <input type="text" />
+      </label>
+      <label>
+        Position Title: <input type="text" />
+      </label>
+      <label>
+        Date of Employment: <input type="text" />
+      </label>
     </div>
   );
 }
