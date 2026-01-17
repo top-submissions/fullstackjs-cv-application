dcb11e430b377f59aef87829849b5a2240c8ed06
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-18 06:56:28 +0800

    feat(form): add submit btn, state and conditional form rendering

--- src/components/forms/GeneralInformationForm/GeneralInformationForm.jsx
+++ src/components/forms/GeneralInformationForm/GeneralInformationForm.jsx
@@ -5,39 +5,55 @@ function GeneralInformationForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+  };
 
   return (
     <div className={styles.formSection}>
       <h2>General Information</h2>
-      <label>
-        Name:{' '}
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label>
-        Email:{' '}
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Phone:{' '}
-        <input
-          type="tel"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
-      </label>
-      <div>
-        <p>{name}</p>
-        <p>{email}</p>
-        <p>{phone}</p>
-      </div>
+      {!isSubmitted ? (
+        <>
+          <label>
+            Name:{' '}
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Email:{' '}
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            Phone:{' '}
+            <input
+              type="tel"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </label>
+          <button onClick={handleSubmit}>Submit</button>
+          <div>
+            <p>{name}</p>
+            <p>{email}</p>
+            <p>{phone}</p>
+          </div>
+        </>
+      ) : (
+        <div>
+          <p>{name}</p>
+          <p>{email}</p>
+          <p>{phone}</p>
+        </div>
+      )}
     </div>
   );
 }
