15f39c58a289e0fd5ac91c8eff8536926b980158
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-18 06:39:04 +0800

    feat(app): add form section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GeneralInformationForm from './components/forms/GeneralInformationForm/GeneralInformationForm';
 import EducationalExperienceForm from './components/forms/EducationalExperienceForm/EducationalExperienceForm';
+import PracticalExperienceForm from './components/forms/PracticalExperienceForm/PracticalExperienceForm';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>CV Application</h1>
       <GeneralInformationForm />
       <EducationalExperienceForm />
+      <PracticalExperienceForm />
     </div>
   );
 }
