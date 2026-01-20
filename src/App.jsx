1e18dc7670b3d7a5f060cfb78dba82aa9ee1b15d
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-20 11:00:43 +0800

    feat(App): add CVPreview component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import styles from './App.module.css';
 import GeneralInformationForm from './components/forms/GeneralInformationForm/GeneralInformationForm';
 import EducationalExperienceForm from './components/forms/EducationalExperienceForm/EducationalExperienceForm';
 import PracticalExperienceForm from './components/forms/PracticalExperienceForm/PracticalExperienceForm';
+import CVPreview from './components/views/CVPreview/CVPreview';
 
 function App() {
   return (
     <div className={styles.appContainer}>
       <h1 className={styles.headerTitle}>CV Application</h1>
-      <GeneralInformationForm />
-      <EducationalExperienceForm />
-      <PracticalExperienceForm />
+      <div className={styles.mainContent}>
+        <div className={styles.formSide}>
+          <GeneralInformationForm />
+          <EducationalExperienceForm />
+          <PracticalExperienceForm />
+        </div>
+        <div className={styles.previewSide}>
+          <CVPreview />
+        </div>
+      </div>
       <button className={styles.printButton} onClick={() => window.print()}>
         Print / Export
       </button>
