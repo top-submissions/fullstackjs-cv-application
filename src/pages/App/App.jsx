0bff7be9ccb40f5db2569e33aaba6773d2ba26ab
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-20 14:44:40 +0800

    feat(context, app): wrap App in GeneralInfoContext

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,21 +5,24 @@ import GeneralInformationForm from '../../components/forms/GeneralInformationFor
 import EducationalExperienceForm from '../../components/forms/EducationalExperienceForm/EducationalExperienceForm';
 import PracticalExperienceForm from '../../components/forms/PracticalExperienceForm/PracticalExperienceForm';
 import CVPreview from '../../components/views/CVPreview/CVPreview';
+import { GeneralInformationProvider } from '../../modules/data/contexts/GeneralInformationContext/GeneralInformationContext';
 
 function App() {
   return (
     <div className={styles.appContainer}>
       <h1 className={styles.headerTitle}>CV Application</h1>
-      <div className={styles.mainContent}>
-        <div className={styles.formSide}>
-          <GeneralInformationForm />
-          <EducationalExperienceForm />
-          <PracticalExperienceForm />
+      <GeneralInformationProvider>
+        <div className={styles.mainContent}>
+          <div className={styles.formSide}>
+            <GeneralInformationForm />
+            <EducationalExperienceForm />
+            <PracticalExperienceForm />
+          </div>
+          <div className={styles.previewSide}>
+            <CVPreview />
+          </div>
         </div>
-        <div className={styles.previewSide}>
-          <CVPreview />
-        </div>
-      </div>
+      </GeneralInformationProvider>
       <button className={styles.printButton} onClick={() => window.print()}>
         Print / Export
       </button>
