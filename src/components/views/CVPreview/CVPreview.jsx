8f6b9f763d9ffb8fb7f1889f34ef7c639c32c9e7
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-21 10:23:10 +0800

    feat(preview): add entries to educExp, uses context provider

--- src/components/views/CVPreview/CVPreview.jsx
+++ src/components/views/CVPreview/CVPreview.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './CVPreview.module.css';
 import { useContext } from 'react';
 import GeneralInformationContext from '../../../modules/data/contexts/GeneralInformationContext';
+import EducationalExperienceContext from '../../../modules/data/contexts/EducationalExperienceContext';
 
 const CVPreview = () => {
   const { generalInformation } = useContext(GeneralInformationContext);
+  const { educationalExperience } = useContext(EducationalExperienceContext);
 
   return (
     <div className={styles.previewContainer}>
@@ -13,6 +15,13 @@ const CVPreview = () => {
       <p>{generalInformation.email}</p>
       <p>{generalInformation.phone}</p>
       <h2>Educational Experience</h2>
+      {educationalExperience.map((entry) => (
+        <div key={entry.id}>
+          <p>{entry.schoolName}</p>
+          <p>{entry.titleOfStudy}</p>
+          <p>{entry.dateOfStudy}</p>
+        </div>
+      ))}
       <h2>Practical Experience</h2>
     </div>
   );
